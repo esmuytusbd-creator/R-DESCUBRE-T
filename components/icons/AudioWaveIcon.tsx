e4e800411e11d8a6517f00f710436dded67db1e7
esmuytusbd-creator <esmuytu.sbd@gmail.com>
2025-09-17 01:10:08 +0200

    Create AudioWaveIcon.tsx

---
+++ components/icons/AudioWaveIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const AudioWaveIcon = ({ className }: { className?: string }): React.ReactNode => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <path d="M2 10v4" />
+    <path d="M6 7v10" />
+    <path d="M10 4v16" />
+    <path d="M14 7v10" />
+    <path d="M18 10v4" />
+    <path d="M22 10v4" />
+  </svg>
+);
+
+export default AudioWaveIcon;
